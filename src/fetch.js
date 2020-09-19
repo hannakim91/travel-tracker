828232e50d4743c0722941f7b5076b933a041b89
Hanna Kim <hannakim91@gmail.com>
2020-09-19 14:05:18 -0600

    Delete unused code in fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,21 +1,18 @@
 const getTravelerData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
     .then(response => response.json())
-    // .then(data => data))
     .catch(err => console.log(err.message));
 };
 
 const getTripData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
     .then(response => response.json())
-    // .then(data => data.tripData)
     .catch(err => console.log(err.message));
 }
 
 const getDestinationData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
     .then(response => response.json())
-    // .then(data => data.destinationsData)
     .catch(err => console.log(err.message));
 }
 
@@ -31,9 +28,6 @@ const getAllData = () => {
     .catch(err => console.log(err.message));
 }
 export default {
-  getTravelerData,
-  getTripData,
-  getDestinationData,
   getAllData
 }
 
