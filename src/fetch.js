4d6ee904c727ede75359b8eb026c48e4006340c0
Hanna Kim <hannakim91@gmail.com>
2020-09-18 19:29:21 -0600

    Write method to fetch traveler data from API

--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,16 @@
-//travelers 
-// https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers
+ 
 
 //trips
 //https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
 
 // dest
 //https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
+const getTravelersData = () => {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
+    .then(response => response.json())
+    .then(data => data.ingredientsData)
+    .catch(err => console.log(err.message))
+};
 
 export default {
   getTravelersData
