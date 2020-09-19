41283ef1a3556e71b79cdf905f035aafbfbc70c4
Hanna Kim <hannakim91@gmail.com>
2020-09-19 12:29:54 -0600

    Write function to get trip data from API

--- src/fetch.js
+++ src/fetch.js
@@ -1,16 +1,20 @@
  
 
-//trips
-//https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
-
 // dest
 //https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
 const getTravelersData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
     .then(response => response.json())
-    .then(data => data.ingredientsData)
+    .then(data => data.userData)
     .catch(err => console.log(err.message))
 };
+
+const getTripData = () => {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
+    .then(response => response.json())
+    .then(data => data.tripData)
+    .catch(err => console.log(err.message))
+}
 
 export default {
   getTravelersData
