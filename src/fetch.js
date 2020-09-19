d78cb128f5a8f1270585e81e65e0db6897c865e4
Hanna Kim <hannakim91@gmail.com>
2020-09-18 19:27:27 -0600

    Import fetch, Traveler, TravelerRepo in index.js

---
+++ src/fetch.js
@@ -0,0 +1,12 @@
+//travelers 
+// https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers
+
+//trips
+//https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
+
+// dest
+//https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
+
+export default {
+  getTravelersData
+}
