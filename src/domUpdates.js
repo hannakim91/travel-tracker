b41aff545d62070db9cb33e20efbeb054f7a9670
Hanna Kim <hannakim91@gmail.com>
2020-09-22 16:31:56 -0600

    Add padding to footer and dashboard

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -82,8 +82,8 @@ const domUpdates = {
     const travelerTripsSection = document.querySelector('.traveler-trip-cards')
     travelerTripsSection.innerHTML += `      
     <article class="traveler-trip-card">
-      <h4>Trip Start : ${trip.date}</h4>
-      <h5>${destinationName}</h5>
+      <h4>${destinationName}</h4>
+      <h5>Trip Start : ${trip.date}</h5>
       <p>Days Traveled: ${trip.duration}</p>
       <p>Travelers: ${trip.travelers}</p>
       <p>Status: ${trip.status}</p>
